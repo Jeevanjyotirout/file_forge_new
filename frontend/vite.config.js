8840f67373c00c8c8f7e0ee1ea8b3d247871e689
Jeevan Jyoti Rout <125113982+Jeevanjyotirout@users.noreply.github.com>
2026-03-12 00:06:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/File_Forge_New/"
   base: "/file_forge_new/"
 })
